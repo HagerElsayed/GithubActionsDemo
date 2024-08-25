d6073e39c2f68897cd52151df46cac53f495b102
Hager Elsayed <hagerelsayed39@gmail.com>
2024-08-25 03:03:59 +0300

    Initial Commit

---
+++ GithubActionsDemo/GithubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsDemoApp.swift
+//  GithubActionsDemo
+//
+//  Created by Hager Elsayed on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
